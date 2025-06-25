fc3d5578aebeb1879431188cea00c56f3f5b7ca1
joemacKE <josephmuga2020@gmail.com>
2025-06-25 14:18:20 +0300

    This file contains python program for creating database alx_book_store and creating tables Books, Authors, Customers, Orders and Order_Details.

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -14,7 +14,6 @@ mycursor.execute("CREATE TABLE IF NOT EXISTS Books (book_id INT PRIMARY KEY, tit
 mycursor.execute("CREATE TABLE IF NOT EXISTS Customers (customer_id INT PRIMARY KEY, " \
 "customer_name VARCHAR(215), email VARCHAR(215), address TEXT)")
 mycursor.execute("CREATE TABLE IF NOT EXISTS Orders (order_id INT PRIMARY KEY, " \
-"customer_id INT, order_date DATE, FOREIGN KEY (customer_id) REFERENCES " \
-"Customers(customer_id))")
+"customer_id INT, order_date DATE, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id))")
 mycursor.execute("CREATE TABLE IF NOT EXISTS Order_Details (orderdetailid INT PRIMARY KEY, order_id INT," \
 "book_id INT, quantity DOUBLE, FOREIGN KEY (order_id) REFERENCES Orders(order_id), FOREIGN KEY (book_id) REFERENCES Books(book_id))")
