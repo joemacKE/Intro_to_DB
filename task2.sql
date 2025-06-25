e46b7114fff4414648dcf89d5bec218b9cfa6d6b
joemacKE <josephmuga2020@gmail.com>
2025-06-25 15:01:38 +0300

    added script to create all the tables

---
+++ task2.sql
@@ -0,0 +1,18 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+host="localhost",
+user="root",
+password="Vision2030$",
+database="alx_book_store"
+)
+mycursor = mydb.cursor()
+mycursor.execute("CREATE TABLE IF NOT EXISTS Authors (author_id INT PRIMARY KEY, author_name VARCHAR(215))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS Books (book_id INT PRIMARY KEY, title VARCHAR(130), " \
+"author_id INT, price DOUBLE, publication_date DATE, FOREIGN KEY (author_id) REFERENCES Authors(author_id))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS Customers (customer_id INT PRIMARY KEY, " \
+"customer_name VARCHAR(215), email VARCHAR(215), address TEXT)")
+mycursor.execute("CREATE TABLE IF NOT EXISTS Orders (order_id INT PRIMARY KEY, " \
+"customer_id INT, order_date DATE, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id))")
+mycursor.execute("CREATE TABLE IF NOT EXISTS Order_Details (orderdetailid INT PRIMARY KEY, order_id INT," \
+"book_id INT, quantity DOUBLE, FOREIGN KEY (order_id) REFERENCES Orders(order_id), FOREIGN KEY (book_id) REFERENCES Books(book_id))")
