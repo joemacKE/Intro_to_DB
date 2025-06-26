e6f2b8b0b9a76649cc70e105061bd3c5a0ffee74
joemacKE <josephmuga2020@gmail.com>
2025-06-26 09:01:16 +0300

    Added script to populate the table customers.

--- task_5.sql
+++ task_5.sql
@@ -1,28 +1,4 @@
-import mysql.connector
-
-try:
-    # Connect to MySQL database
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Vision2030$",
-        database="alx_book_store"
-    )
-
-    cursor = conn.cursor()
-    sql = "INSERT INTO Customers (customer_id, customer_name, email, address) VALUES (%s, %s, %s, %s)"
-    val = 1, "Cole Baido", "cbaidoo@sandtech.com", "123 Happiness Ave."
-    cursor.execute(sql, val)
-    tables = cursor.fetchall()
-
-    for (table,) in tables:
-        print(table)
-
-except mysql.connector.Error as err:
-    print("Error:", err)
-
-finally:
-    if 'cursor' in locals():
-        cursor.close()
-    if 'conn' in locals() and conn.is_connected():
-        conn.close()
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, "Cole BaidoO", "cbaidoo@sandtech.com", "123 Happiness Ave.")
